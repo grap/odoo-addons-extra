fa86165ab886292eacd1bba831cfcfbfc26c1161
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-02-19 12:21:13 +0100

    [TEST] modules to test decorators_allowed_groups

---
+++ decorator_allowed_groups_module_A/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2021-Today: GRAP (<http://www.grap.coop/>)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': "Test Module A",
+    'summary': """Dummy module for test""",
+    'author': 'GRAP, Odoo Community Association (OCA)',
+    'website': "https://github.com/OCA/server-tools/",
+    'category': 'Technical',
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'depends': [
+        'purchase',
+        'decorator_allowed_groups',
+    ],
+    'installable': True
+}
